73a8ff17ae0f42a2f5f513b7ec476a62c16c581e
AwiYunawan <hisagikurokiba@gmail.com>
2025-04-09 23:04:26 +0700

    feat: integrate Firebase SDK and configure Firestore and Auth
    - Added Firebase SDK to package.json
    - Updated tsconfig.json to include Firebase paths
    - Created FirebaseConfig.ts for initializing Firebase app, Firestore, and Auth
    - Configured Metro bundler in metro.config.js

---
+++ FirebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { initializeAuth, getReactNativePersistence} from "firebase/auth";
+import { ReactNativeAsyncStorage } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1fDxosZN5D47FCS6fW5y77bWj3Kfu0xY",
+  authDomain: "aplikasi-kasir-94c93.firebaseapp.com",
+  projectId: "aplikasi-kasir-94c93",
+  storageBucket: "aplikasi-kasir-94c93.firebasestorage.app",
+  messagingSenderId: "532168021359",
+  appId: "1:532168021359:web:117598cbf462d3b107849a",
+  measurementId: "G-QB2B7EXYL8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence});
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,35 @@
-import { Redirect } from "expo-router";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import { Auth } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { router } from "expo-router";
 
-export default function Index() {
-  return <Redirect href="/transaksi" />;
-}
+const index = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  
+
+  const signIn = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      if (user) router.push("/transaksi");
+    } catch (error: any) {
+      console.error(error);
+      alert("Sign in failed: " + error.message);
+    }
+  };
+  const signUp = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);}
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>index</Text>
+    </SafeAreaView>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
