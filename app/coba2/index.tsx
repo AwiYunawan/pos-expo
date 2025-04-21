ab239956111da9875c5447ffdbef23f21dfd06bd
AwiYunawan <hisagikurokiba@gmail.com>
2025-04-21 23:48:28 +0700

    chore: add firebase-admin and firebase-function dependencies to package.json

--- app/coba2/index.tsx
+++ app/coba2/index.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View , Button} from "react-native";
 import React from "react";
 
 const index = () => {
+  const handleUpload = () => {
+    console.log("Upload button pressed");
+  };
+
+  const handleDownload = () => {
+    console.log("Download button pressed");
+  };
+
   return (
     <View>
       <Text>ini halaman untuk coba storage</Text>
+      <Text>Upload File to Firebase Storage</Text>
+      <Button title="Upload File" onPress={handleUpload} />
+      <Text>Download File from Firebase Storage</Text>
+      <Button title="Download File" onPress={handleDownload} />
     </View>
   );
 };
